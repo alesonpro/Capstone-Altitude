790d400e3e6e666ae77ff313c09831921734393a
JayAldrin <j-p0371@students.uc-bcf.edu.ph>
2024-02-20 21:51:55 +0800

    minor changes

--- display_archived_data.php
+++ display_archived_data.php
@@ -24,7 +24,7 @@ $result = mysqli_query($connection, $query);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Logs Page</title>
+    <title>Archived Logs Page</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
