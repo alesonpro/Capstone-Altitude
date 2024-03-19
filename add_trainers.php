958d8b2fa86b27cf60804d6809d8aa2b10d4da8a
aleson pro <rolfaleson.pro@gmail.com>
2024-03-19 20:36:50 +0800

    fixed add trainer layout

--- add_trainers.php
+++ add_trainers.php
@@ -121,16 +121,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
         <div class="form-group">
             <label for="specialty">Specialty:</label>
-            <input type="text" id="specialty" name="specialty" placeholder="Calisthenics">
+            <select name="specialty" id="specialty">
+                <option value="Bodybuilding">BodyBuilding</option>
+                <option value="Calisthenics">Calisthenics</option>
+                <option value="Body Recomposition">Body Recomposition</option>
+                <option value="Weight Loss">Weight Loss</option>
+            </select>
+            <!-- <input type="text" id="specialty" name="specialty" placeholder="Calisthenics"> -->
         </div>
+
         <div class="form-group">
             <label for="schedule_start">Schedule Start:</label>
-            <input type="text" id="schedule_start" name="schedule_start" placeholder="Monday">
+            <select id="schedule_start" name="schedule_start">
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+                <option value="Sunday">Sunday</option>
+            </select>
         </div>
         <div class="form-group">
             <label for="schedule_end">Schedule End:</label>
-            <input type="text" id="schedule_end" name="schedule_end" placeholder="Saturday">
+            <select id="schedule_end" name="schedule_end">
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+                <option value="Sunday">Sunday</option>
+            </select>
         </div>
+
         <div class="form-group">
             <label for="time_in">Time-in:</label>
             <input type="time" id="time_in" name="time_in" class="form-control" required>
